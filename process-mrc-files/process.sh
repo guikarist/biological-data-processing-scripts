bae38d1a79ad1d0ee993108baa2db99de05f6945
guikarist <guikarist@gmail.com>
2019-12-12 10:46:27 +0800

    Format shell scripts

--- process-mrc-files/process.sh
+++ process-mrc-files/process.sh
@@ -15,48 +15,48 @@ files=(./*.mrc)
 
 file_1=${files[0]}
 
-IFS=$seq read -ra names <<< "$file_1"
+IFS=$seq read -ra names <<<"$file_1"
 min="${names[$deg_pos]}"
 max="${names[$deg_pos]}"
 
 for ((i = 1; i < ${#files[@]}; ++i)); do
     file_2=${files[$i]}
 
-    IFS=$sep read -ra names_1 <<< "$file_1"
-    IFS=$sep read -ra names_2 <<< "$file_2"
-    
-    # Find duplicates.
+    IFS=$sep read -ra names_1 <<<"$file_1"
+    IFS=$sep read -ra names_2 <<<"$file_2"
+
+    # Find duplicates
     if [ "${names_1[$name_pos]}" == "${names_2[$name_pos]}" -a "${names_1[$deg_pos]}" == "${names_2[$deg_pos]}" ]; then
         mv $file_1 ./duplicates
-        echo "Find duplicate: $file_1" >> log/duplicates.txt
+        echo "Find duplicate: $file_1" >>log/duplicates.txt
 
-	file_1=$file_2
+        file_1=$file_2
         continue
     fi
 
     if [ "${names_1[$name_pos]}" == "${names_2[$name_pos]}" ]; then
-        # Update the maximum and minimum.
-	if (( $(echo "${names_1[$deg_pos]} > ${names_2[$deg_pos]}" | bc -l) )); then
-	    min="${names_2[$deg_pos]}"
-	fi
-	if (( $(echo "${names_1[$deg_pos]} < ${names_2[$deg_pos]}" | bc -l) )); then
-	    max="${names_2[$deg_pos]}"
-	fi
+        # Update the maximum and minimum
+        if (($(echo "${names_1[$deg_pos]} > ${names_2[$deg_pos]}" | bc -l))); then
+            min="${names_2[$deg_pos]}"
+        fi
+        if (($(echo "${names_1[$deg_pos]} < ${names_2[$deg_pos]}" | bc -l))); then
+            max="${names_2[$deg_pos]}"
+        fi
     else
-        # Process the group of data.
-        echo "${names_1[$name_pos]},${min},${max}" >> log/ranges.txt
-        newstack `ls ${names_1[$name_pos]}_* | sort -nr -k 4 -t '_'` "stack-out/${names_1[$name_pos]}.mrc"
+        # Process the group of data
+        echo "${names_1[$name_pos]},${min},${max}" >>log/ranges.txt
+        newstack $(ls ${names_1[$name_pos]}_* | sort -nr -k 4 -t '_') "stack-out/${names_1[$name_pos]}.mrc"
         sh -c "mv ${names_1[$name_pos]}_* ./backup"
 
-	max="${names_2[$deg_pos]}"
-	min="${names_2[$deg_pos]}"
+        max="${names_2[$deg_pos]}"
+        min="${names_2[$deg_pos]}"
     fi
 
     file_1=$file_2
 done
 
-# Process the last group of data.
-IFS=$sep read -ra names <<< "$file_1"
-echo "${names[$name_pos]},${min},${max}" >> log/ranges.txt
-newstack `ls ${names[$name_pos]}_* | sort -nr -k 4 -t '_'` "stack-out/${names[$name_pos]}.mrc"
+# Process the last group of data
+IFS=$sep read -ra names <<<"$file_1"
+echo "${names[$name_pos]},${min},${max}" >>log/ranges.txt
+newstack $(ls ${names[$name_pos]}_* | sort -nr -k 4 -t '_') "stack-out/${names[$name_pos]}.mrc"
 sh -c "mv ${names[$name_pos]}_* ./backup"
